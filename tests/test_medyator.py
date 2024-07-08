b0d1b8e7283a1564332af39c61e1e98e951d1e93
misdirection <misdirection@live.de>
2024-07-08 20:53:18 +0200

    updated tests

--- tests/test_medyator.py
+++ tests/test_medyator.py
@@ -1,13 +1,15 @@
 import os
 import sys
 
+from medyator.errors import HandlerNotFound
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pytest
 from medyator.medyator import Medyator
 from medyator.contracts import Query, Command
 from medyator.request_handler import QueryHandler, CommandHandler
 
-from kink import di
+from kink import di, inject
 import medyator.kink  # for extension of Container
 
 
@@ -30,6 +32,7 @@ def test_command():
         def __init__(self, value: str) -> None:
             self.value = value
 
+    @inject(alias=TestCommand)
     class TestCommandHandler(CommandHandler[TestCommand]):
         def __init__(self) -> None:
             self.value = None
@@ -40,8 +43,7 @@ def test_command():
     return TestCommand, TestCommandHandler()
 
 
-def test_query_handler(test_query):
-
+def test_query_should_execute_query_handler_twice_and_return_same_value(test_query):
     di.add_medyator()
     query, query_handler = test_query
     di[query] = query_handler
@@ -54,3 +56,29 @@ def test_query_handler(test_query):
     # should get handler from medyator container
     result = medyator.send_query(query(1))
     assert result == 9001
+
+
+def test_should_execute_command_handler_and_set_the_value(test_command):
+    di.add_medyator()
+    command, _ = test_command
+    medyator = di[Medyator]
+
+    medyator.send_command(command("Hello, World!"))
+    handler = di[command]
+    assert handler.value == "Hello, World!"
+
+
+def test_should_raise_HandlerNotFound_error_when_query_handler_is_not_found(test_query):
+    di.add_medyator()
+    medyator = di[Medyator]
+    query, _ = test_query
+    with pytest.raises(HandlerNotFound):
+        medyator.send_query(query(1))
+
+
+def test_should_raise_HandlerNotFound_error_when_command_handler_is_not_found(test_command):
+    di.add_medyator()
+    medyator = di[Medyator]
+    command, _ = test_command
+    with pytest.raises(HandlerNotFound):
+        medyator.send_command(command("Hello, World!"))
